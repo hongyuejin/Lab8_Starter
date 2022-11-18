b6987d8571335160967951a96b6e3b1ae5be9140
Hong <hojin@ucsd.edu>
2022-11-17 18:44:23 -0800

    Added all the required files for this assignment

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lab 8 - Starter
+
+### Grace Jin (worked individually)
+
+### Where would you fit your automated tests in your Recipe project development pipeline?
+Within a Github action that runs whenever code is pushed. As mentioned in the previous lab, we can see Github action as a event-listener, which it gets automatically triggered when we do certain actions like pull request, issues, and etc. By fitting our automated tests to the github action and let it be triggered everytime we push to the repo, we can achieve the goal of always testing for quality code and maintaining our general project structure while continuously integrating features/changes to our repo. </br>
+
+### Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+No.</br>
+
+### Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+No. I think UI test would be better to test this feature since write/send a message is not a single function operation but a series of user actions. Since unit test cannot test how these individual components interact with each other on an application/feature level, it's not applicable to use unit test here. </br>
+
+### Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+Yes. We can achieve checking the max message length by using regular expressions to do unit tests. </br>
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,97 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Test 1- phone number
+// Correct-output
+test('858-532-2312 is a valid phone number', () => {
+	expect(functions.isPhoneNumber("858-532-2312")).toBe(true); 
+});
+
+test('958-532-2222 is a valid phone number', () => {
+	expect(functions.isPhoneNumber("958-532-2222")).toBe(true); 
+});
+
+// Wrong-output
+test('934-342-234Q is not a valid phone number', () => {
+	expect(functions.isPhoneNumber("934-342-234Q")).toBe(false); 
+});
+
+test('3444-233-555 is not a valid phone number', () => {
+	expect(functions.isPhoneNumber("3444-233-555")).toBe(false); 
+});
+
+// Test 2- Email
+// Correct-output
+test('me@p.zP is a valid email', () => {
+	expect(functions.isEmail("me@p.zP")).toBe(true); 
+});
+
+test('wait@m.acc is a valid email', () => {
+	expect(functions.isEmail(`wait@m.acc`)).toBe(true); 
+});
+
+// Wrong-output
+test('9+2@qq.com is not a valid email', () => {
+	expect(functions.isEmail("9+2@qq.com")).toBe(false); 
+});
+
+test('\\\\@ucsb.edu is not a valid email', () => {
+	expect(functions.isEmail("\\\\@ucsb.edu")).toBe(false); 
+});
+
+// Test 3- Password
+// Correct-output
+test('lll3000r is a valid strong password', () => {
+	expect(functions.isStrongPassword("lll3000r")).toBe(true); 
+});
+
+test('p_r32iii2 is a valid strong password', () => {
+	expect(functions.isStrongPassword(`p_r32iii2`)).toBe(true); 
+});
+
+// Wrong-output
+test('12345 is not a valid strong password', () => {
+	expect(functions.isStrongPassword("12345")).toBe(false); 
+});
+
+test('1+2=10 is not a valid strong password', () => {
+	expect(functions.isStrongPassword("1+2=10")).toBe(false); 
+});
+
+// Test 4- Date
+// Correct-output
+test('11/60/2000 is a valid date', () => {
+	expect(functions.isDate("11/60/2000")).toBe(true); 
+});
+
+test('99/20/5000 is a valid date', () => {
+	expect(functions.isDate(`99/20/5000`)).toBe(true); 
+});
+
+// Wrong-output
+test('1/2/3 is not a valid date', () => {
+	expect(functions.isDate("1/2/3")).toBe(false); 
+});
+
+test('1/01/201 is not a valid date', () => {
+	expect(functions.isDate("1/01/201")).toBe(false); 
+});
+
+// Test 5- HexColor
+// Correct-output
+test('AAC2FF is a valid hex color', () => {
+	expect(functions.isHexColor("AAC2FF")).toBe(true); 
+});
+
+test('AAC2FF is a valid hex color', () => {
+	expect(functions.isHexColor("AAC2FF")).toBe(true); 
+});
+
+// Wrong-output
+test('M02 is not a valid hex color', () => {
+	expect(functions.isHexColor("M02")).toBe(false); 
+});
+
+test('JQK is not a valid hex color', () => {
+	expect(functions.isHexColor("JQK")).toBe(false); 
+});
